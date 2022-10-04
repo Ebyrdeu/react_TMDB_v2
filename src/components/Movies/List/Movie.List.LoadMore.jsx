6c44acebb2841ca89edeee06401bafd2a7ec6dc2
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2022-10-04 12:53:21 +0200

    ###ADDED --- Load more movies on scroll hit bottom

---
+++ src/components/Movies/List/Movie.List.LoadMore.jsx
@@ -0,0 +1,36 @@
+import {IconChevronsDown} from "@tabler/icons";
+import {createStyles, Text} from "@mantine/core";
+import {useEffect} from "react";
+
+const useStyles = createStyles(() => ({
+	wrapper: {
+		userSelect: "none",
+		display: "flex",
+		justifyContent: "center",
+		flexDirection: "column",
+		alignItems: "center",
+		marginBottom: 20,
+	},
+
+}));
+
+export const LoadMore = ({next}) => {
+	const {classes} = useStyles(undefined, undefined);
+
+	// Detect Bottom of screen
+	const onFindBottom = (e) => (window.innerHeight + e.target.documentElement.scrollTop >= e.target.documentElement.scrollHeight) ? next() : null;
+
+	// Effects
+	useEffect(() => {
+		window.addEventListener("scroll", onFindBottom);
+		return () => window.removeEventListener("scroll", onFindBottom);
+	});
+
+	return (
+		<div className={classes.wrapper}>
+			<Text color={"dimmed"} children={"Scroll down to load more movies"}/>
+			<IconChevronsDown/>
+		</div>
+	);
+};
+
