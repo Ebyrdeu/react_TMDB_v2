1eb62348f8eadbe2253883b0bb888f4237c47ec4
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2022-10-05 20:46:21 +0200

    ###Fixed --- Back button bug, when rerender whole page on it

--- src/components/SingleMovie/SingleMovie.component.jsx
+++ src/components/SingleMovie/SingleMovie.component.jsx
@@ -1,4 +1,4 @@
-import {Link, useParams} from "react-router-dom";
+import {Link, useNavigate, useParams} from "react-router-dom";
 import {useShowSingleMovie} from "../../hooks/useShowSingleMovie.js";
 import {Button, Container, Image} from "@mantine/core";
 import {Top} from "./SingleMovie.Top";
@@ -9,33 +9,28 @@ export const SingleMovie = () => {
 
 	const {id} = useParams();
 	const {data, isLoading} = useShowSingleMovie(id);
-
+	const navigate = useNavigate();
 
 	if (isLoading) return;
 
 	return (
 		<>
 			<Image src={`https://image.tmdb.org/t/p/w500/${data?.data.backdrop_path}`} height={400}/>
-			<Link to={`/`}>
 				<Button
+					onClick={() => navigate(-1)}
 					variant="filled"
 					leftIcon={<IconArrowLeft size={20} stroke={2}/>}
 					radius="xs"
 					size="md"
 					styles={{
 						root: {
-							marginLeft: 10,
-							paddingRight: 14,
-							height: 48,
-							position: "absolute",
-							top: '375px',
-							right: '20px',
+							marginLeft: 10, paddingRight: 14, height: 48,
+							position: "absolute", top: '375px', right: '20px',
 							zIndex: 9999
 						},
 						rightIcon: {marginLeft: 5},
 					}}
 					children={'Back'}/>
-			</Link>
 			<Container size={"xl"}>
 				<Top data={data}/>
 				<Bottom/>
