c41b67da154c5945c1e12b60219d222ed1e3ab39
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2022-10-11 22:03:15 +0200

    ### Changed --- added small settings vor vite

--- src/components/single/actor/SingleActor.Component.jsx
+++ src/components/single/actor/SingleActor.Component.jsx
@@ -7,38 +7,38 @@ import {useShowSingleActor} from "../../../hooks/useShowSingleActor.js";
 import {useActorComponentStyles} from "./styled-components/SingleActor.Component.Styles.js";
 
 export const SingleActor = () => {
-	const {id} = useParams();
-	const {data, isLoading} = useShowSingleActor(id);
-	const navigate = useNavigate();
-	const {classes} = useActorComponentStyles(undefined, undefined);
+    const {id} = useParams();
+    const {data, isLoading} = useShowSingleActor(id);
+    const navigate = useNavigate();
+    const {classes} = useActorComponentStyles(undefined, undefined);
 
-	if (isLoading) return;
+    if (isLoading) return;
 
-	return (
-		<>
+    return (
+        <>
 
-			<Button
-				onClick={() => navigate(-1)}
-				variant="filled"
-				leftIcon={<IconArrowLeft size={20} stroke={2}/>}
-				radius="xs"
-				size="md"
-				styles={{
-					root: {
-						marginLeft: 10, paddingRight: 14, height: 48,
-						position: "absolute", top: "75px", right: "20px",
-						zIndex: 9999,
-					},
-					rightIcon: {marginLeft: 5},
-				}}
-				children={"Back"}/>
+            <Button
+                onClick={() => navigate(-1)}
+                variant="filled"
+                leftIcon={<IconArrowLeft size={20} stroke={2}/>}
+                radius="xs"
+                size="md"
+                styles={{
+                    root: {
+                        marginLeft: 10, paddingRight: 14, height: 48,
+                        position: "absolute", top: "75px", right: "20px",
+                        zIndex: 9999,
+                    },
+                    rightIcon: {marginLeft: 5},
+                }}
+                children={"Back"}/>
 
-			<div className={classes.bg}/>
-			<Container size={"xl"}>
-				<Top data={data}/>
-				<Bottom/>
-			</Container>
-		</>
-	);
+            <div className={classes.bg}/>
+            <Container size={"xl"}>
+                <Top data={data}/>
+                <Bottom/>
+            </Container>
+        </>
+    );
 };
 
